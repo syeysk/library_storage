fd8738e15206df089bb782d20ee6765f86ba5308
shyzik93 <shyzik93@mail.ru>
2022-07-15 04:28:30 +0300

    refactor

--- tests/test_core.py
+++ tests/test_core.py
@@ -45,38 +45,6 @@ ORIGIN_DIFF_CSV = (
     'Новый,,directory03/file08.txt\r\n'
 )
 
-COPY_DEL_FS = (
-    ('/copy/file01.txt', 'content01'),
-    ('/copy/file05.txt', 'content05'),
-    ('/copy/directory01/file03.txt', 'content03'),
-    ('/copy/directory01/file04.txt', 'content04'),
-    ('/copy/directory02/file06.txt', 'content06'),
-    ('/copy/directory03/file08.txt', 'content08'),
-)
-COPY_DEL_DB = [
-    ('4256508e9e2099aa72050b5e00d01745153971e915fa190e4a86079a13ab8e73', 1, '', 'file01.txt', 0),
-    ('982dfb44d32c54183e5399ae180a701d70c1434736645eea98c23e6a81b99d1b', 2, '', 'file02.txt', 1),
-    ('ecefb01d5d2f2cce6d2f51257e4d17b480aec572496cb6b2136740704529f35f', 3, '', 'file05.txt', 0),
-    ('78f690041494259dbb0ca8e890b9464599f5fc1eeaad20de7c4c16b7761c0039', 4, 'directory01', 'file03.txt', 0),
-    ('90acddd186ee9932103da80265a716c2340cbaeeecaaa1da18b78d8e02f97e72', 5, 'directory01', 'file04.txt', 0),
-    ('186bf48b6b0d045b57d29f6961eb7fd10710cfdcd745384ba0c8015e11eb2363', 6, 'directory02', 'file06.txt', 0),
-    ('30273fefb11ee96b052b34051985952ff551b77bde97ba11c79b7b8ab3dc2a16', 7, 'directory02', 'file07.txt', 1),
-    ('5491aedaa8d92e62f28117a5d5a751c31c917ed05ffd974cd433fd5a9834c554', 8, 'directory03', 'file08.txt', 0)
-]
-COPY_DEL_DIFF_CSV = (
-    'Удалён,file02.txt,,982dfb44d32c54183e5399ae180a701d70c1434736645eea98c23e6a81b99d1b,2\r\n'
-    'Удалён,directory02/file07.txt,,30273fefb11ee96b052b34051985952ff551b77bde97ba11c79b7b8ab3dc2a16,7\r\n'
-)
-
-ORIGIN_AFTER_COPY_DEL_DB = [
-    ('4256508e9e2099aa72050b5e00d01745153971e915fa190e4a86079a13ab8e73', 1, '', 'file01.txt', 0),
-    ('ecefb01d5d2f2cce6d2f51257e4d17b480aec572496cb6b2136740704529f35f', 3, '', 'file05.txt', 0),
-    ('78f690041494259dbb0ca8e890b9464599f5fc1eeaad20de7c4c16b7761c0039', 4, 'directory01', 'file03.txt', 0),
-    ('90acddd186ee9932103da80265a716c2340cbaeeecaaa1da18b78d8e02f97e72', 5, 'directory01', 'file04.txt', 0),
-    ('186bf48b6b0d045b57d29f6961eb7fd10710cfdcd745384ba0c8015e11eb2363', 6, 'directory02', 'file06.txt', 0),
-    ('5491aedaa8d92e62f28117a5d5a751c31c917ed05ffd974cd433fd5a9834c554', 8, 'directory03', 'file08.txt', 0)
-]
-
 
 class CoreTestCase(TestCase):
     def create_files(self, files_and_content):
@@ -94,23 +62,8 @@ class CoreTestCase(TestCase):
         self.origin_ls.__exit__(None, None, None)
         self.copy_ls.__exit__(None, None, None)
 
-    def test_scan_to_db(self):
-        self.origin_ls.scan_to_db(library_path='/origin')
-        data_origin = self.origin_ls.db.cu.execute('select * from files').fetchall()
-        self.assertEqual(ORIGIN_DB, data_origin)
-
-    def test_scan_to_db_with_diff(self):
-        self.origin_ls.scan_to_db(library_path='/origin')
-        data_origin = self.origin_ls.db.cu.execute('select * from files').fetchall()
-        self.assertEqual(ORIGIN_DB, data_origin)
-        self.origin_ls.save_diff(library_path='/origin', diff_file_path='/diff.zip')
-        with zipfile.ZipFile('/diff.zip', 'r') as diff_zip:
-            diff_zip.testzip()
-            with diff_zip.open(self.origin_ls.ARCHIVE_DIFF_FILE_NAME, 'r') as diff_file:
-                self.assertEqual(ORIGIN_DIFF_CSV, str(diff_file.read(), 'utf-8'))
-
-    def test_all_process_delete_file(self):
-        self.create_files(COPY_DEL_FS)
+    def all_process(self, copy_fs, copy_db, copy_diff_csv, origin_db_after_applying_diff):
+        self.create_files(copy_fs)
 
         self.origin_ls.scan_to_db(library_path='/origin')
         data_origin = self.origin_ls.db.cu.execute('select * from files').fetchall()
@@ -127,14 +80,62 @@ class CoreTestCase(TestCase):
         self.copy_ls.scan_to_db(library_path='/copy')
         print(self.copy_ls.diffs)
         data_copy = self.copy_ls.db.cu.execute('select * from files').fetchall()
-        self.assertEqual(COPY_DEL_DB, data_copy)
+        self.assertEqual(copy_db, data_copy)
 
         self.copy_ls.save_diff(library_path='/copy', diff_file_path='/diff.zip')
         with zipfile.ZipFile('/diff.zip', 'r') as diff_zip:
             diff_zip.testzip()
             with diff_zip.open(self.copy_ls.ARCHIVE_DIFF_FILE_NAME, 'r') as diff_file:
-                self.assertEqual(COPY_DEL_DIFF_CSV, str(diff_file.read(), 'utf-8'))
+                self.assertEqual(copy_diff_csv, str(diff_file.read(), 'utf-8'))
 
         self.origin_ls.apply_diff(library_path='/origin', diff_file_zip_path='/diff.zip')
         data_origin = self.origin_ls.db.cu.execute('select * from files').fetchall()
-        self.assertEqual(ORIGIN_AFTER_COPY_DEL_DB, data_origin)
+        self.assertEqual(origin_db_after_applying_diff, data_origin)
+
+    def test_scan_to_db(self):
+        self.origin_ls.scan_to_db(library_path='/origin')
+        data_origin = self.origin_ls.db.cu.execute('select * from files').fetchall()
+        self.assertEqual(ORIGIN_DB, data_origin)
+
+    def test_scan_to_db_with_diff(self):
+        self.origin_ls.scan_to_db(library_path='/origin')
+        data_origin = self.origin_ls.db.cu.execute('select * from files').fetchall()
+        self.assertEqual(ORIGIN_DB, data_origin)
+        self.origin_ls.save_diff(library_path='/origin', diff_file_path='/diff.zip')
+        with zipfile.ZipFile('/diff.zip', 'r') as diff_zip:
+            diff_zip.testzip()
+            with diff_zip.open(self.origin_ls.ARCHIVE_DIFF_FILE_NAME, 'r') as diff_file:
+                self.assertEqual(ORIGIN_DIFF_CSV, str(diff_file.read(), 'utf-8'))
+
+    def test_all_process_delete_file(self):
+        copy_fs = (
+            ('/copy/file01.txt', 'content01'),
+            ('/copy/file05.txt', 'content05'),
+            ('/copy/directory01/file03.txt', 'content03'),
+            ('/copy/directory01/file04.txt', 'content04'),
+            ('/copy/directory02/file06.txt', 'content06'),
+            ('/copy/directory03/file08.txt', 'content08'),
+        )
+        copy_db = [
+            ('4256508e9e2099aa72050b5e00d01745153971e915fa190e4a86079a13ab8e73', 1, '', 'file01.txt', 0),
+            ('982dfb44d32c54183e5399ae180a701d70c1434736645eea98c23e6a81b99d1b', 2, '', 'file02.txt', 1),
+            ('ecefb01d5d2f2cce6d2f51257e4d17b480aec572496cb6b2136740704529f35f', 3, '', 'file05.txt', 0),
+            ('78f690041494259dbb0ca8e890b9464599f5fc1eeaad20de7c4c16b7761c0039', 4, 'directory01', 'file03.txt', 0),
+            ('90acddd186ee9932103da80265a716c2340cbaeeecaaa1da18b78d8e02f97e72', 5, 'directory01', 'file04.txt', 0),
+            ('186bf48b6b0d045b57d29f6961eb7fd10710cfdcd745384ba0c8015e11eb2363', 6, 'directory02', 'file06.txt', 0),
+            ('30273fefb11ee96b052b34051985952ff551b77bde97ba11c79b7b8ab3dc2a16', 7, 'directory02', 'file07.txt', 1),
+            ('5491aedaa8d92e62f28117a5d5a751c31c917ed05ffd974cd433fd5a9834c554', 8, 'directory03', 'file08.txt', 0)
+        ]
+        copy_diff_csv = (
+            'Удалён,file02.txt,,982dfb44d32c54183e5399ae180a701d70c1434736645eea98c23e6a81b99d1b,2\r\n'
+            'Удалён,directory02/file07.txt,,30273fefb11ee96b052b34051985952ff551b77bde97ba11c79b7b8ab3dc2a16,7\r\n'
+        )
+        origin_db_after_applying_diff = [
+            ('4256508e9e2099aa72050b5e00d01745153971e915fa190e4a86079a13ab8e73', 1, '', 'file01.txt', 0),
+            ('ecefb01d5d2f2cce6d2f51257e4d17b480aec572496cb6b2136740704529f35f', 3, '', 'file05.txt', 0),
+            ('78f690041494259dbb0ca8e890b9464599f5fc1eeaad20de7c4c16b7761c0039', 4, 'directory01', 'file03.txt', 0),
+            ('90acddd186ee9932103da80265a716c2340cbaeeecaaa1da18b78d8e02f97e72', 5, 'directory01', 'file04.txt', 0),
+            ('186bf48b6b0d045b57d29f6961eb7fd10710cfdcd745384ba0c8015e11eb2363', 6, 'directory02', 'file06.txt', 0),
+            ('5491aedaa8d92e62f28117a5d5a751c31c917ed05ffd974cd433fd5a9834c554', 8, 'directory03', 'file08.txt', 0)
+        ]
+        self.all_process(copy_fs, copy_db, copy_diff_csv, origin_db_after_applying_diff)
