1ca2bd1f841bc600f417ba8ba50f6745e9523bae
shyzik93 <shyzik93@mail.ru>
2022-10-05 00:19:47 +0300

    add export for Markdown

--- library_storage.py
+++ library_storage.py
@@ -27,6 +27,43 @@ def get_file_hash(file_path):
     return hasher.hexdigest()
 
 
+class CSVExporter:
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+        self.csv_writer = None
+        self.csv_file = None
+
+    def open_new_page(self, csv_current_page):
+        csv_full_path = os.path.join(self.csv_path, '{}.csv'.format(str(csv_current_page)))
+        self.csv_file = open(csv_full_path, 'w', encoding='utf-8', newline='\n')
+        self.csv_writer = csv.writer(self.csv_file)
+
+    def write_row(self, row):
+        self.csv_writer.writerow(row)
+
+    def close(self):
+        self.csv_file.close()
+
+
+class MarkdownExporter:
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+        self.csv_file = None
+
+    def open_new_page(self, csv_current_page):
+        TABLE_HOLDER = 'Описание | Ссылка на книгу\n--- | ---\n'
+        csv_full_path = os.path.join(self.csv_path, '{}.md'.format(str(csv_current_page)))
+        self.csv_file = open(csv_full_path, 'w', encoding='utf-8')
+        self.csv_file.write(TABLE_HOLDER)
+
+    def write_row(self, row):
+        TABLE_ROW = '[{id}](книга_{id}) | [{name}](D://Книги/{name}/{pathdir})\n'
+        self.csv_file.write(TABLE_ROW.format(id=row[1], name=row[3], pathdir=row[2]))
+
+    def close(self):
+        self.csv_file.close()
+
+
 class DBStorage:
     COUNT_ROWS_FOR_INSERT = 30
     COUNT_ROWS_ON_PAGE = 10
@@ -260,34 +297,30 @@ class LibraryStorage:
         Экспортирует из базы метаинформацию в CSV без заголовков. Формат строки следующий:
         хэш,идентификатор,директория,имя файла
         """
-        csv_writer = None
-        csv_file = None
-        csv_current_page = 0
+        exporter = CSVExporter(csv_path)
+
+        csv_current_page = 1
+        exporter.open_new_page(csv_current_page)
         number_of_last_row_on_current_page = self.CSV_COUNT_ROWS_ON_PAGE
         count_rows = self.db.get_count_rows() if progress_count_exported_files else None
         number_of_current_row = None
         for number_of_current_row, row in enumerate(self.db.select_rows()):
             number_of_last_row_on_current_page = number_of_last_row_on_current_page - row[1] + 1
-            if csv_writer is None or number_of_current_row == number_of_last_row_on_current_page:
-                if csv_file:
-                    csv_file.close()
-
+            if number_of_current_row == number_of_last_row_on_current_page:
+                exporter.close()
                 number_of_last_row_on_current_page += self.CSV_COUNT_ROWS_ON_PAGE
                 csv_current_page += 1
-                csv_full_path = os.path.join(csv_path, '{}.csv'.format(str(csv_current_page)))
-                csv_file = open(csv_full_path, 'w', encoding='utf-8', newline='\n')
-                csv_writer = csv.writer(csv_file)
+                exporter.open_new_page(csv_current_page)
                 if progress_count_exported_files:
                     progress_count_exported_files(number_of_current_row + 1, count_rows, csv_current_page)
 
-            csv_writer.writerow(row)
+            exporter.write_row(row)
             number_of_last_row_on_current_page += row[1]
 
         if progress_count_exported_files:
             progress_count_exported_files(number_of_current_row + 1, count_rows, csv_current_page)
 
-        if csv_file:
-            csv_file.close()
+        exporter.close()
 
     def import_csv_to_db(self, csv_path):
         for csv_filename in os.scandir(csv_path):
